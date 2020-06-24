251e1ddcf78ce55ee73eeace2848c46ffb2acf3d
Elnaz Babayeva <elnaz.babayeva@avast.com>
2020-06-24 12:53:37 +0200

    DB insert

--- DB/propaganda_db.py
+++ DB/propaganda_db.py
@@ -1,5 +1,6 @@
 
 import sqlite3
+import sqlalchemy
 
 class DB:
     def __init__(self, db_file):
@@ -8,37 +9,52 @@ class DB:
         self.c = self.conn.cursor()
 
     def url_exist(self, url):
-        return self.c.execute(f'''SELECT EXISTS(SELECT 1 FROM URLS WHERE url={url});''')
+        url_exist = db.c.execute("""SELECT url_id FROM URLS WHERE url=(?);""", (url,))
+        if len(url_exist.fetchall()) > 0:
+            return True
+        return False
+    def link_exist(self, parent_id, child_id):
+        url_exist = db.c.execute("""SELECT LINK_ID FROM LINKS WHERE parent_id=(?) and child_id=(?);""", (parent_id,child_id))
+        if len(url_exist.fetchall()) > 0:
+            return True
+
+        return False
 
     def get_url_key(self, url):
-        return self.c.execute(f'''SELECT ID FROM URLS WHERE url={url};''')
+        ids = self.c.execute("""SELECT URL_ID FROM URLS WHERE url=(?)""", (url, )).fetchall()
+        if len(ids) > 1:
+            raise Exception("There are multiple ids for this url")
+        if len(ids) == 0:
+            raise Exception("URLS is not in DB")
+        else:
+            return ids[0][0]
 
 
     def insert_url(self, url, content=None, date_published=None, date_of_query=None):
         url_exist = self.url_exist(url)
         if not url_exist:
             self.c.execute(f"""INSERT INTO URLS(url, content, date_published, date_of_query) VALUES 
-            ({url}, {content}, {date_published}, {date_of_query}) """)
+            (?, ?, ?, ?) """, (url, content, date_published, date_of_query))
             self.commit()
 
-    def insert_link_id(self,parent_id,child_id,  source=None):
+    def insert_link_id(self, parent_id, child_id,  source=None):
         self.c.execute(f"""INSERT INTO LINKS(parent_id, child_id, source) VALUES 
-                    ({parent_id}, {child_id}, {source}) """)
+                    (?, ?, ?) """, (parent_id, child_id,  source))
         self.commit()
-
 
 
     def insert_link_urls(self, parent_url, child_url, source):
         parent_url_exist = self.url_exist(parent_url)
         child_url_exist = self.url_exist(child_url)
-        if parent_url_exist:
+        if not parent_url_exist:
             self.insert_url(parent_url)
-        if child_url_exist:
+        if not child_url_exist:
             self.insert_url(child_url)
 
-        child_url_key = self.get_url_key(child_url)
-        parent_url_key = self.get_url_key(parent_url)
-        self.insert_link_id(parent_url_key, child_url_key, source)
+        child_id = self.get_url_key(child_url)
+        parent_id = self.get_url_key(parent_url)
+        if not db.link_exist(child_id, parent_id):
+            self.insert_link_id(child_id, parent_id, source)
 
     def commit(self):
         self.conn.commit()
@@ -47,5 +63,21 @@ class DB:
         self.conn.close()
 
 
+if __name__ == '__main__':
+    db = DB("propaganda.db")
+    s = 'sebas.html'
+    e = "elnaz.html"
+    db.insert_url(s)
+    db.insert_url(e)
+    db.insert_url(e)
+    print(db.c.execute("""SELECT * FROM URLS;""").fetchall())
+    db.insert_link_urls(s,e, "G")
+    print(db.c.execute("""SELECT * FROM LINKS;""").fetchall())
+
+    print(db.get_url_key(s))
+    # #db.insert_url()
+    #
+    # print('INSERT INTO URLS(url) VALUES {}'.format(s.replace('"', '""')))
+    #
 
 
