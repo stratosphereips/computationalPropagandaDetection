2b05a6f6a0995881e5e7bf83b1cb1b28298cfb4e
Sebastian Garcia <eldraco@gmail.com>
2020-06-29 18:26:14 +0200

    Better filtering of URLs with a small blacklist and making of the graph

--- web-network-mapper.py
+++ web-network-mapper.py
@@ -2,14 +2,16 @@
 
 import sys
 from datetime import datetime
-import time
 import requests
 from DB.propaganda_db import DB
 import traceback
 from serpapi.google_search_results import GoogleSearchResults
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
+# Read the serapi api key
 f = open('serapi.key')
 SERAPI_KEY = f.readline()[:-1]
 f.close()
@@ -24,6 +26,16 @@ def trigger_api(search_leyword):
     Yandex using YandexSearchResults class
     GoogleScholar using GoogleScholarSearchResults class
 
+    This api has
+    "position": 8,
+    "title": "COVID-19 и Мишель Фуко (некоторые мысли вслух ...",
+    "link": "https://www.geopolitica.ru/article/covid-19-i-mishel-fuko-nekotorye-mysli-vsluh",
+    "displayed_link": "www.geopolitica.ru › article › covid-...",
+    "thumbnail": null,
+    "date": "Apr 5, 2020",
+    "snippet": "... проект ID2020 (почитать о нем можно здесь - https://www.fondsk.ru/news/2020/03/25/borba-s-koronavirusom-i-bolshoj-brat-50441.html).",
+    "cached_page_link": "https://webcache.googleusercontent.com/search?q=cache:rrzBOTKH5BsJ:https://www.geopolitica.ru/article/covid-19-i-mishel-fuko-nekotorye-mysli-vsluh+&cd=8&hl=en&ct=clnk&gl=us"
+
     """
     try:
         params = {
@@ -34,6 +46,11 @@ def trigger_api(search_leyword):
                 }
         client = GoogleSearchResults(params)
         results = client.get_dict()
+        # Store the results of the api for future comparison
+        modificator_time = str(datetime.now().hour) + ':' + str(datetime.now().minute) + ':' + str(datetime.now().second)
+        # write the results to a json file so we dont lose them
+        with open('results-' + modificator_time + '.json', 'w') as f:
+            json.dump(results, f)
         return results
     except Exception as e:
         print('Error in trigger_api()')
@@ -48,7 +65,8 @@ class URLs():
         self.db = DB(file_db)
 
     def set_child(self, parent: str, child: str):
-        print(f'\tNew children in object: {parent} -> {child}')
+        #print(f'\tNew children in object: {parent} -> {child}')
+        print(f'\tNew children in object: > {child}')
         self.urls[parent]['children'] = child
         self.db.insert_link_urls(parent_url=parent, child_url=child, source="G")
 
@@ -105,6 +123,15 @@ def downloadContent(url):
 
 if __name__ == "__main__":
     try:
+        # Create the dot object to plot a graph
+        G = nx.Graph()
+        # G = nx.cubical_graph()
+        # Labels for the nx graph
+        labels = {}
+
+        # Blacklist of pages to ignore
+        blacklist = {'robots.txt', 'sitemap.xml'}
+
         # Receive the first URL to search
         search_string = ' '.join(sys.argv[1:])
 
@@ -119,10 +146,23 @@ if __name__ == "__main__":
         iteration = 0
         # Get everything
         for url in urls_to_search:
-            print(f'Searching data for url: {url}')
-            URLs.add_url(url)
+            # For now, dont go in an infinite search for the Internet.
+            if iteration == 30:
+                break
             # Keep track of the iteration level
             iteration += 1
+
+            print('\n==========================================')
+            print(f'Searching data for url: {url}')
+
+            # Add this url
+            URLs.add_url(url)
+
+            # Add the node
+            G.add_node(url)
+            # Add the label
+            labels[url] = url.split('/')[2]
+
             # Get the content of the url and store it
             content = downloadContent(url)
             URLs.store_content(url, content)
@@ -130,28 +170,70 @@ if __name__ == "__main__":
             # Get other links to this URL for the next round (children)
             data = trigger_api(url)
             urls = []
-            for result in data['organic_results']:
-                urls.append(result['link'])
+            # From all the jsons, get only the links to continue
+            try:
+                for result in data['organic_results']:
+                    urls.append(result['link'])
+            except KeyError:
+                # There are no organic_results
+                pass
 
             for children_url in urls:
+
+                # Apply some black list of the pages we dont want, such as
+                # sitemap.xml and robots.txt , because they only have links
+                # The page is the text after the last /
+                if children_url.split('/')[-1] in blacklist:
+                    continue
+
+                # Add the node
+                G.add_node(children_url)
+                # Add the label
+                labels[children_url] = children_url.split('/')[2]
+
+                # Add the edge
+                G.add_edge(url, children_url)
+
                 # Add the children to the DB
                 URLs.set_child(url, children_url)
+
+                # Set the datetime for the url
+                # For now it is a string like "Mar 25, 2020"
+                result_date = ''
+                try:
+                    result_date = result['date']
+                except KeyError:
+                    # There is no date field in the results
+                    result_date = ''
+                URLs.set_datetime(url, result_date)
+
+                # Set the time we asked for the results
+                result_search_date = datetime.now()
+                URLs.set_search_datetime(url, result_search_date)
+
                 # Check that the children was not seen before in this call
                 try:
                     _ = urls_to_search.index(children_url)
                     # We hav, so ignore
-                    print(f'Repeated url: {children_url}')
+                    print(f'\tRepeated url: {children_url}')
                 except ValueError:
                     # The url was not in the list. Append
                     urls_to_search.append(children_url)
                 except Exception as e:
                     print(f"Error type {type(e)}")
                     print(f"Error {e}")
-            print(f'Urls after searching for th {iteration} URL: {urls_to_search}')
+            print(f'\tUrls after searching for th {iteration} URL: {urls_to_search}')
 
         print('Finished with all the graph of URLs')
         print('Final list of urls')
         URLs.show_urls()
+
+        # Generate the graph
+        nx.draw(G, labels=labels, with_labels=True)
+        # nx.draw_spectral(G)
+        # pos = nx.spring_layout(G)
+        # nx.draw_networkx_labels(G, pos, labels, font_size=16)
+        plt.show()
     except Exception as e:
         print(f"Error in main(): {e}")
         print(f"{type(e)}")
