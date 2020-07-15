cecb7a2ec90aa8377f00040f70e839c97b3473c0
Elnaz Babayeva <elnaz.babayeva@avast.com>
2020-07-15 17:31:03 +0200

    Supporting dates and saving them in DB

--- urls.py
+++ urls.py
@@ -7,11 +7,11 @@ class URLs:
         self.db = DB(file_db)
         self.verbosity = verbosity
 
-    def set_child(self, parent: str, child: str):
+    def set_child(self, parent: str, child: str, link_date):
         if self.verbosity > 2:
             print(f"\tNew children in object: > {child}")
         self.urls[parent]["children"] = child
-        self.db.insert_link_urls(parent_url=parent, child_url=child, source="G")
+        self.db.insert_link_urls(parent_url=parent, child_url=child, link_date=link_date, source="G")
 
     def store_content(self, url: str, content: str):
         if self.verbosity > 2:
@@ -32,14 +32,16 @@ class URLs:
         self.urls[url] = {}
         self.db.insert_url(url=url, is_propaganda=is_propaganda)
 
-    def set_datetime(self, url, datetime):
+    def set_datetime(self, url:str, datetime:str):
         """
         Set the datetime when the result was published
         """
         self.urls[url] = {"publication_date": datetime}
+        self.db.update_date_of_query(url, datetime)
 
-    def set_search_datetime(self, url, result_search_date):
+    def set_query_datetime(self, url:str, result_search_date:str):
         """
         Set the datetime when we searched for this
         """
         self.urls[url] = {"search_date": result_search_date}
+        self.db.update_date_of_query(url, result_search_date)
