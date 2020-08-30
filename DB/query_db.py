db2d328a2143008527755219f32f13a5768c0b4b
Sebastian Garcia <eldraco@gmail.com>
2020-08-30 14:08:37 +0200

    Add function to list all the links with nice colors from a url

--- DB/query_db.py
+++ DB/query_db.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--link", help="URL to ask to the DB.",
                         type=str, required=True)
+    parser.add_argument("-L", "--links", help="List all the links.",
+                        type=str, required=False)
     parser.add_argument("-v", "--verbosity", help="Verbosity level",
                         type=int, default=0)
     parser.add_argument("-d", "--path_to_db", help="DB file",
@@ -17,15 +19,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
     try:
         db = DB(args.path_to_db)
-        all_links = db.get_tree(args.link)
-        previous_parent = ''
-        for result in all_links:
-            if previous_parent == result[0]:
-                print(f'\t{Fore.YELLOW}-> {result[1]}{Style.RESET_ALL}')
-            else:
-                print(f'{Fore.CYAN}{result[0]}{Style.RESET_ALL}')
-                print(f'\t{Fore.YELLOW}-> {result[1]}{Style.RESET_ALL}')
-                previous_parent = result[0]
+
+        if args.link:
+            all_links = db.get_tree(args.link)
+            previous_parent = ''
+            for result in all_links:
+                if previous_parent == result[0]:
+                    print(f'\t{Fore.YELLOW}-> {result[1]}{Style.RESET_ALL}')
+                else:
+                    print(f'{Fore.CYAN}{result[0]}{Style.RESET_ALL}')
+                    print(f'\t{Fore.YELLOW}-> {result[1]}{Style.RESET_ALL}')
+                    previous_parent = result[0]
+        elif args.links:
+            pass
+
     except KeyboardInterrupt:
         # If ctrl-c is pressed
         pass
