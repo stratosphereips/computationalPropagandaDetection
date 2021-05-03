adace6c22411cec66b7e7c6e433ccfb323660ead
Sebastian Garcia <eldraco@gmail.com>
2021-05-03 14:47:38 +0200

    Only twitter now for test

--- web-network-mapper.py
+++ web-network-mapper.py
@@ -171,7 +171,8 @@ if __name__ == "__main__":
                 all_urls_by_urls, all_urls_by_titles = [], []
                 # Search by link
                 print(f"\n{Fore.CYAN}== Level {level}. Google search by LINKS to {url}{Style.RESET_ALL}")
-                google_results_urls = search_google_by_link(url, URLs)
+                #google_results_urls = search_google_by_link(url, URLs)
+                google_results_urls = []
                 all_urls_by_urls.extend(google_results_urls)
 
                 print(f"\n{Fore.BLUE}== Level {level}. Twitter search by LINKS as {url}{Style.RESET_ALL}")
@@ -179,19 +180,22 @@ if __name__ == "__main__":
                 all_urls_by_urls.extend(twitter_results_urls)
 
                 print(f"\n{Fore.GREEN}== Level {level}. VK search by LINKS as {url}{Style.RESET_ALL}")
-                vk_results_urls = extract_and_save_vk_data(URLs, url, url, "link")
+                # vk_results_urls = extract_and_save_vk_data(URLs, url, url, "link")
+                vk_results_urls = []
                 all_urls_by_urls.extend(vk_results_urls)
 
                 # Search by Title
                 if title is not None:
                     print(f"\n{Fore.CYAN}== Level {level}. Google search by TITLE as {title}{Style.RESET_ALL}")
-                    google_results_urls_title = search_google_by_title(title, url, URLs)
+                    # google_results_urls_title = search_google_by_title(title, url, URLs)
+                    google_results_urls_title = []
                     all_urls_by_titles.extend(google_results_urls_title)
                     print(f"\n{Fore.BLUE}== Level {level}. Twitter search by title as {title}{Style.RESET_ALL}")
                     twitter_results_urls_title = extract_and_save_twitter_data(URLs, title, url, "title")
                     all_urls_by_titles.extend(twitter_results_urls_title)
                     print(f"\n{Fore.GREEN}== Level {level}. VK search by title as {title}{Style.RESET_ALL}")
-                    vk_results_urls_title = extract_and_save_vk_data(URLs, title, url, "title")
+                    #vk_results_urls_title = extract_and_save_vk_data(URLs, title, url, "title")
+                    vk_results_urls_title = []
                     all_urls_by_titles.extend(vk_results_urls_title)
 
                 urls_to_search_by_level[level + 1] = all_urls_by_urls
