111bdd5c6802ccbeb53b93212cb5fd46b19fd98f
Ondrej Boucek <ondra.boucek@gmail.com>
2022-01-18 10:40:05 +0100

    Change sys.stdout to log file in dataset_downloader

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -12,7 +12,7 @@ def main(data, idx_from):
     for idx in range(idx_from, len(data)):
         data_root = '/data/propagandadetection/EuVsDisinfo_dataset/'
 
-        # sys.stdout = open(f'{data_root}logs/{idx}.log', 'w')
+        sys.stdout = open(f'{data_root}logs/{idx}.log', 'w')
         link = data[idx]
         with open(f'{data_root}logs/dataset_downloader.log', 'a') as log_file:
             try:
@@ -27,7 +27,7 @@ def main(data, idx_from):
             graph.create_domain_centered(link, db_path, graph_name=domain_graph_name)
             timeline_graph_name = data_root + f'graphs/timeline_{idx}.html'
             graph.create_date_centered(link, db_path, graph_name=timeline_graph_name)
-        # sys.stdout.close()
+        sys.stdout.close()
 
 
 if __name__ == '__main__':
