60f2dd029348518b2c7ddcd5034d9d076eff35d8
Ondrej Boucek <ondra.boucek@gmail.com>
2022-01-31 13:38:01 +0100

    Add argument to redirect output to log file

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -8,11 +8,11 @@ import graph
 wnm = importlib.import_module("web-network-mapper")
 
 
-def main(data, idx):
+def main(data, idx, redirect):
     # for idx in range(idx_from, len(data)):
     data_root = '/data/propagandadetection/EuVsDisinfo_dataset/'
-
-    sys.stdout = open(f'{data_root}logs/{idx}.log', 'w')
+    if redirect:
+        sys.stdout = open(f'{data_root}logs/{idx}.log', 'w')
     link = data[idx]
     with open(f'{data_root}logs/dataset_downloader.log', 'a') as log_file:
         try:
@@ -27,7 +27,8 @@ def main(data, idx):
         graph.create_domain_centered(link, db_path, graph_name=domain_graph_name)
         timeline_graph_name = data_root + f'graphs/timeline_{idx}.html'
         graph.create_date_centered(link, db_path, graph_name=timeline_graph_name)
-    sys.stdout.close()
+    if redirect:
+        sys.stdout.close()
 
 
 if __name__ == '__main__':
@@ -38,8 +39,9 @@ if __name__ == '__main__':
     parser.add_argument(
         "-u", "--url_file", help="txt file containing urls", type=str, required=False, default="propaganda_data.txt"
     )
+    parser.add_argument("-r", "--redirect", help="redirect output to log file", action='store_true')
     args = parser.parse_args()
     with open(args.url_file) as file:
         all_links = file.read().splitlines()
         print(len(all_links))
-    main(all_links, args.from_idx)
+    main(all_links, args.from_idx, args.redirect)
